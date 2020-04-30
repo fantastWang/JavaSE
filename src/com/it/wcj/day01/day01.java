21db191e58b3f49f07640d36a2af6e320072ec62
fantast <1061505447@qq.com>
2020-04-30 15:35:26 +0800

    init file day01-02

---
+++ src/com/it/wcj/day01/day01.java
@@ -0,0 +1,10 @@
+package com.it.wcj.day01;
+
+/**
+ * @author wangchaojie
+ * @Description TODO
+ * @Date 2020/4/30 14:45
+ **/
+public class day01 {
+
+}
---
+++ src/com/it/wcj/day02/day02.java
@@ -0,0 +1,9 @@
+package com.it.wcj.day02;
+
+/**
+ * @author wangchaojie
+ * @Description TODO
+ * @Date 2020/4/30 15:30
+ **/
+public class day02 {
+}
