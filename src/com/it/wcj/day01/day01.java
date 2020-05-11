bafde2525418693e71342f45c60bc74f51067978
fantast <1061505447@qq.com>
2020-05-11 17:23:10 +0800

    不同进制之间的表示

--- src/com/it/wcj/day01/day01.java
+++ src/com/it/wcj/day01/day01.java
@@ -17,7 +17,7 @@ public class day01 {
      * @Date 15:10 2020/5/9
      **/
     @Test
-    public void test01() {
+    public void mathOperation() {
         byte b = 10;
         char c = 'a';
         short s = 20;
@@ -28,4 +28,37 @@ public class day01 {
         //short+char
         int i2 = s + c;
     }
+
+    /**
+      * @Author wangchaojie
+      * @Description 输出* *
+      * @Date 13:19 2020/5/11
+      **/
+    @Test
+    public void stringOperation() {
+        //单引号是char字符，使用+会变成算术运算
+        System.out.println('*'+'\t'+'*');
+        //当+前后有一个是字符串时，+就代表时连接符，而不是运算符
+        System.out.println('*'+"\t"+'*');
+    }
+
+    /**
+      * @Author wangchaojie
+      * @Description 不同进制之间的表示
+      * @Date 16:25 2020/5/11
+      **/
+    @Test
+    public void intoSystem(){
+        //二进制，以0b或0B开头
+        int a1 = 0b10;
+        //八进制，以0开头
+        int a2 = 010;
+        //十进制
+        int a3 = 10;
+        //十六进制，以0x或0X开头
+        int a4 = 0x10;
+        System.out.println(a1+"-"+a2+"-"+a3+"-"+a4);
+    }
+
+
 }
