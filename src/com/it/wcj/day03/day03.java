86e735e4edffac846c13d83d05ab3fb6406d1834
fantast <1061505447@qq.com>
2020-05-13 17:30:42 +0800

    提交代码

--- src/com/it/wcj/day03/day03.java
+++ src/com/it/wcj/day03/day03.java
@@ -9,6 +9,11 @@ import org.junit.jupiter.api.Test;
  **/
 public class day03 {
 
+    /**
+      * @Author wangchaojie
+      * @Description 当switch case的语句一致时，可以考虑进行合并，减少冗余
+      * @Date 17:30 2020/5/13
+      **/
     @Test
     public void test01(){
 
