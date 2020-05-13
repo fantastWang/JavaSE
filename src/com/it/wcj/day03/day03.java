847f7943bd1b4dba19a7e9cf575f1ae0dc5ac51f
18010913593 <1061505447@qq.com>
2020-05-13 22:02:27 +0800

    switch语句

--- src/com/it/wcj/day03/day03.java
+++ src/com/it/wcj/day03/day03.java
@@ -15,7 +15,21 @@ public class day03 {
       * @Date 17:30 2020/5/13
       **/
     @Test
-    public void test01(){
-
+    public void switchOperation(){
+        String str = "123";
+        switch (str){
+            case "1":
+                System.out.println("1");
+                break;
+            case "2":
+                System.out.println("2");
+                break;
+            case "3":
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("4");
+                break;
+        }
     }
 }
